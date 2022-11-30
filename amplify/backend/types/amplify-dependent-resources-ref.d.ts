7ebd15b7f0bb4ccec21d68dd604b45c9802ccaf9
Sudha Arumugam <arumsudu@amazon.co.uk>
2022-11-30 09:32:44 +0000

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "storage": {
+        "amplifystoretage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
